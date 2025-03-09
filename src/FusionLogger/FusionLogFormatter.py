16bb257213ff4c45341cb4bd5ca45ff3713ddcd9
Lasse27 <llhillen1403@gmail.com>
2025-03-09 13:10:19 +0100

    (Add): Added LogFormatter class and slight reformatting

---
+++ src/FusionLogger/FusionLogFormatter.py
@@ -0,0 +1,3 @@
+class FusionLogFormatter(object):
+    def __init__(self):
+        pass
--- src/FusionLogger/FusionLogger.py
+++ src/FusionLogger/FusionLogger.py
@@ -1,61 +1,109 @@
 import inspect
+import sys
 from types import FrameType
 from typing import cast
 
 from FusionLogLevel import FusionLogLevel
+from FusionLogFormatter import FusionLogFormatter
 
 
 class FusionLogger(object):
 
     def __init__(self):
-        pass
-
-    def __str__(self):
-        pass
+        self.MinLogLevel = FusionLogLevel.Info
+        self.Formatter = FusionLogFormatter()
 
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # Fundamental methods
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    def Debug(self, message):
+    def debug(self, message):
         pass
 
-    def Info(self, message):
+    def info(self, message):
         pass
 
-    def Warning(self, message: str, exception: Exception):
+    def warning(self, message: str, exception: Exception):
         pass
 
-    def Critical(self, message: str, exception: Exception):
+    def critical(self, message: str, exception: Exception):
         pass
 
-    def BeginScope(self, scope: str):
+    def begin_scope(self, scope: str):
         pass
 
-    def EndScope(self, scope: str):
+    def end_scope(self, scope: str):
         pass
 
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # Inner methods
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    def __Log(self, level: FusionLogLevel, message: str, exception: Exception):
+    def __log(self, level: FusionLogLevel, message: str, exception: Exception):
+        """
+
+        """
+
+        # Erstellung des
         pass
 
-    @staticmethod
-    def demo_the_caller_name() -> tuple[str, str]:
-        """Return the calling function's name."""
-        # Ref: https://stackoverflow.com/a/57712700/
-        method: str = cast(FrameType, cast(FrameType, inspect.currentframe()).f_back).f_code.co_name
-        clazz: str = cast(FrameType, cast(FrameType, inspect.currentframe()).f_back).f_code.co_qualname
-        return method, clazz
+
+class FusionLoggerBuilder(object):
+    def __init__(self):
+        self.Logger = FusionLogger()
+
+    def set_name(self, name: str):
+        self.Logger.Name = name
+        return self
+
+    def set_loglevel(self, level: FusionLogLevel):
+        self.Logger.MinLogLevel = level
+        return self
+
+    def set_formatter(self, formatter: FusionLogFormatter):
+        self.Logger.Formatter = formatter
+        return self
+
+    def build(self):
+        return self.Logger
+
+
+def caller_info(skip=2):
+    """Get the name of a caller in the format module.class.method.
+    Copied from: https://gist.github.com/techtonik/2151727
+    :arguments:
+        - skip (integer): Specifies how many levels of stack
+                          to skip while getting caller name.
+                          skip=1 means "who calls me",
+                          skip=2 "who calls my caller" etc.
+    :returns:
+        - package (string): caller package.
+        - module (string): caller module.
+    """
+    stack = inspect.stack()
+    start = 0 + skip
+    if len(stack) < start + 1:
+        return ''
+    parentframe = stack[start][0]
+
+    # module and packagename.
+    package: str = ""
+    module: str = ""
+    module_info = inspect.getmodule(parentframe)
+    if module_info:
+        mod = module_info.__name__.split('.')
+        package: str = mod[0]
+        module: str = mod[1]
+
+    # Remove reference to frame
+    # See: https://docs.python.org/3/library/inspect.html#the-interpreter-stack
+    del parentframe
+    return package, module
+
 
 def deepened():
-    (method, clazz) =FusionLogger.demo_the_caller_name()
-    print(clazz)
-    print(method)
+    FusionLogger.module_from_stack()
+
 
 if __name__ == "__main__":
     deepened()
-
-
