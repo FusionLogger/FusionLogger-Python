6393e658e0891992a760aebd1089fdc3593ad3e4
Lasse27 <llhillen1403@gmail.com>
2025-03-09 15:03:46 +0100

    (Add:) Full documentation for loglevel finsished

--- src/FusionLogger/FusionLogLevel.py
+++ src/FusionLogger/FusionLogLevel.py
@@ -2,16 +2,52 @@ from enum import Enum
 
 
 class FusionLogLevel(Enum):
-    """ TODO: DOCUMENTATION """
+    """
+    Definiert die Schweregrade für Logmeldungen in einem mehrstufigen Logging-System.
+
+    Attributes:
+        Debug: Niedrigste Stufe für Entwicklerdiagnosen
+        Info: Allgemeine Systeminformationen
+        Warning: Potenzielle Probleme
+        Critical: Kritische Systemfehler
+    """
 
     Debug = 0,
-    """ TODO: DOCUMENTATION """
+    """
+    Verwendet für detaillierte Debug-Informationen während der Entwicklung.
+
+    Typische Anwendungen:
+        - Variablenzustände
+        - Ablaufverfolgung
+        - Temporäre Diagnoseausgaben
+    """
 
     Info = 1,
-    """ TODO: DOCUMENTATION """
+    """
+    Beschreibt reguläre Systemoperationen.
+
+    Wichtige Ereignisse:
+        - Erfolgreiche Transaktionen
+        - Systemstart/Shutdown
+        - Konfigurationsänderungen
+    """
 
     Warning = 2,
-    """ TODO: DOCUMENTATION """
+    """
+    Kennzeichnet potenzielle Problemstellen.
+
+    Anwendungsfälle:
+        - Unerwartete aber behandelbare Zustände
+        - Deprecation-Hinweise
+        - Ressourcenengpässe
+    """
 
     Critical = 3
-    """ TODO: DOCUMENTATION """
+    """
+    Dokumentiert schwerwiegende Systemfehler.
+
+    Wird verwendet bei:
+        - Nicht behandelbaren Ausnahmen
+        - Datenverlusten
+        - Kritischen Abhängigkeitsfehlern
+    """
